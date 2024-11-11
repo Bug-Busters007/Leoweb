b3f2c9c0c1838235a0d5ed842cf0a7f16f00f4c5
Manuel Groß <m.gross@HTBLALeonding.onmicrosoft.com>
2024-11-11 22:47:57 +0100

    test

---
+++ Leoweb/Leoweb.Server/Controllers/DataController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Leoweb.Server.Controllers;
+
+public class DataController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
