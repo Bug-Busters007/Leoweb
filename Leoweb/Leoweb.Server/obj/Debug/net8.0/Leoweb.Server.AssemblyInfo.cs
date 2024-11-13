bc72b1166a336f0a2f077f91a9d038513c044fc7
LangederDavid <d.langeder@students.htl-leonding.ac.at>
2024-11-13 12:02:36 +0100

    homepage style updated

--- Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
+++ Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f250f23dfeadb02c0b4ddfdabd5a34708f98e431")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efbe6f3f5c3139f0452f7b26dcd4c94a08aafed4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
