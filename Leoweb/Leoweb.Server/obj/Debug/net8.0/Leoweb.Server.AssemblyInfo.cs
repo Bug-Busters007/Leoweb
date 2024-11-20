ea54737e4686335a3af3ec94bf25880423352cff
Manuel Groß <m.gross@HTBLALeonding.onmicrosoft.com>
2024-11-20 22:00:04 +0100

    datenbankverbindung und docs erweiterung

--- Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
+++ Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f85c8b45f6954092342fa7919ae8c10679061cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5943d49961f2f8f89ba77154e8a2f414c65e9b76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
