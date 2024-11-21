b7409387691026cdb66790c34d59b6a7db174748
Michael Oswald <145994175+OswaldMichael2008@users.noreply.github.com>
2024-11-21 12:18:02 +0100

    hab verändert weil bildschirm daheim größer und war iwie trotzdem verschoben aber jetz sollte so halbwegs passen

--- Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
+++ Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b8f0b5d023d81e025765551e3fc8f3e822e6120")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a0e1a3cecb786a38a79c021da3ab8dc4a388f21")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
