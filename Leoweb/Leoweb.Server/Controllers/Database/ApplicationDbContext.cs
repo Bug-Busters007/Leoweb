5058d8b63bdc863896ddf606ddd7743ebce52af1
Michael Oswald <145994175+OswaldMichael2008@users.noreply.github.com>
2024-11-25 21:00:30 +0100

    schwendt seine fehler korrigieren

--- Leoweb/Leoweb.Server/Controllers/Database/ApplicationDbContext.cs
+++ Leoweb/Leoweb.Server/Controllers/Database/ApplicationDbContext.cs
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-﻿using Leoweb.Server.Controllers.Database;
+using Leoweb.Server.Controllers.Database;
 using Leoweb.Server.Controllers.Database.Models;
-=======
-﻿using Leoweb.Server.Controllers.Database.Models;
->>>>>>> 7591059a793893f6fb6dff24120e227a92981830
+using Leoweb.Server.Controllers.Database.Models;
 using Microsoft.EntityFrameworkCore;
 
 public class ApplicationDbContext : DbContext
