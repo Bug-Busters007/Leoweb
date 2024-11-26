475970f42ba5f6e9cc417c54383b455ee4612490
SchwendtbauerJulian <j.schwendtbauer@students.htl-leonding.ac.at>
2024-11-26 08:20:49 +0100

    push build

--- Leoweb/Leoweb.Server/Controllers/Database/ApplicationDbContext.cs
+++ Leoweb/Leoweb.Server/Controllers/Database/ApplicationDbContext.cs
@@ -1,6 +1,5 @@
 using Leoweb.Server.Controllers.Database;
 using Leoweb.Server.Controllers.Database.Models;
-using Leoweb.Server.Controllers.Database.Models;
 using Microsoft.EntityFrameworkCore;
 
 public class ApplicationDbContext : DbContext
