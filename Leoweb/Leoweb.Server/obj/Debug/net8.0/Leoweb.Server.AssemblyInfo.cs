058b20f56518291f79d39e60d420d842e5aee983
GrossManuelHTL <m.gross@htblaleonding.onmicrosoft.com>
2024-11-26 08:01:06 +0100

    Subjects updated

--- Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
+++ Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbebe6b71872da8791c6982720f941ceab2d61f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5058d8b63bdc863896ddf606ddd7743ebce52af1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
