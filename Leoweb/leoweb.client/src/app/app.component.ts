2c622893edc2d48782003b883a69197322d33982
Michael Oswald <145994175+OswaldMichael2008@users.noreply.github.com>
2024-12-03 19:37:23 +0100

    header component funktioniert, module component fast fertig

--- Leoweb/leoweb.client/src/app/app.component.ts
+++ Leoweb/leoweb.client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component, signal} from '@angular/core';
 import {RouterModule, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./components/header/header.component";
+import {ModuleComponent} from "./components/module/module.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, HeaderComponent],
+  imports: [RouterModule, HeaderComponent, ModuleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Leoweb/leoweb.client/src/app/components/module/module.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-module',
+  templateUrl: './module.component.html',
+  styleUrl: './module.component.css',
+  standalone: true,
+})
+export class ModuleComponent {
+
+}
