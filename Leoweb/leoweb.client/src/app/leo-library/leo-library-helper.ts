13cba808848aea53d928c95f0817188100e3a6fa
SchwendtbauerJulian <j.schwendtbauer@students.htl-leonding.ac.at>
2025-01-06 20:23:21 +0100

    add getSubjects helper method

---
+++ Leoweb/leoweb.client/src/app/leo-library/leo-library-helper.ts
@@ -0,0 +1,8 @@
+import { HttpClient } from "@angular/common/http";
+import { ApiService } from "../../services/api.service";
+
+export async function getAllSubjectsFromBranch(http: HttpClient, apiService: ApiService, branch: string) :Promise<[] | undefined> {
+  const url = apiService.getApiUrl(`Notes/allSubjectsFromBranch?branch=${branch}`);
+  const response = await http.get<[]>(url).toPromise();
+  return response;
+}
