05ad67d43a2fc18fd9bb7c3ad7da3b2be744985c
MoertendorferLukas <l.moertendorfer@students.htl-leonding.ac.at>
2025-01-07 09:36:06 +0100

    Fixed Component

--- Leoweb/leoweb.client/src/app/account-settings/account-settings.component.ts
+++ Leoweb/leoweb.client/src/app/account-settings/account-settings.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-account-settings',
   templateUrl: './account-settings.component.html',
+  standalone: true,
   styleUrl: './account-settings.component.css'
 })
 export class AccountSettingsComponent {
