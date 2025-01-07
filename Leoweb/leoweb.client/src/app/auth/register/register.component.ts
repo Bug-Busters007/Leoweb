666600d5ce816c7b4249d4a26ea98e571adfeadf
MoertendorferLukas <l.moertendorfer@students.htl-leonding.ac.at>
2025-01-07 11:26:09 +0100

    Fixed Component

--- Leoweb/leoweb.client/src/app/auth/register/register.component.ts
+++ Leoweb/leoweb.client/src/app/auth/register/register.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
+  standalone: true,
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
