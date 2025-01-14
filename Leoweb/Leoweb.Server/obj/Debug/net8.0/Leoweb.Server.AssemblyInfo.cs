ad7348d45f38921263887eb13cf90fe8902ed528
LangederDavid <d.langeder@students.htl-leonding.ac.at>
2025-01-14 10:11:35 +0100

    removed pdf template directory

--- Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
+++ Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+303e5406d8f4c8c0f2fee7e955f34b0c83cf115c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2bf05970b03faf30dfda530475ef66345b3a9af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
