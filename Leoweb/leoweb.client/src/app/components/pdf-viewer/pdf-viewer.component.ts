1b7588e95a9d47e569e6b4df721abfb55542db4e
LangederDavid <d.langeder@htblaleonding.onmicrosoft.com>
2025-01-19 20:08:04 +0100

    added style for pdf viewer

--- Leoweb/leoweb.client/src/app/components/pdf-viewer/pdf-viewer.component.ts
+++ Leoweb/leoweb.client/src/app/components/pdf-viewer/pdf-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pdf-viewer',
@@ -9,30 +9,57 @@ import { Component} from '@angular/core';
 export class PdfViewerComponent {
   public static showPdf(url: string) {
     console.log('PdfViewerComponent.showPdf');
+
+    // Modal Container
     const modalContainer = document.createElement('div');
     modalContainer.style.position = 'fixed';
     modalContainer.style.top = '0';
     modalContainer.style.left = '0';
     modalContainer.style.width = '100%';
     modalContainer.style.height = '100%';
-    modalContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    modalContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
     modalContainer.style.display = 'flex';
     modalContainer.style.justifyContent = 'center';
     modalContainer.style.alignItems = 'center';
     modalContainer.style.zIndex = '1000';
     document.body.appendChild(modalContainer);
 
+    // PDF Embed Element
     const embedElement = document.createElement('embed');
     embedElement.style.width = '80%';
     embedElement.style.height = '80%';
+    embedElement.style.borderRadius = '10px';
+    embedElement.style.border = '2px solid orangered';
     embedElement.src = url;
     modalContainer.appendChild(embedElement);
 
+    // Close Button
     const closeModalButton = document.createElement('button');
     closeModalButton.textContent = 'Close';
     closeModalButton.style.position = 'absolute';
     closeModalButton.style.top = '10px';
     closeModalButton.style.right = '10px';
+    closeModalButton.style.padding = '5px 20px';
+    closeModalButton.style.fontFamily = 'Consolas, sans-serif';
+    closeModalButton.style.fontSize = '1.5rem';
+    closeModalButton.style.cursor = 'pointer';
+    closeModalButton.style.border = '2px solid orangered';
+    closeModalButton.style.borderRadius = '10px';
+    closeModalButton.style.backgroundColor = 'white';
+    closeModalButton.style.color = 'orangered';
+    closeModalButton.style.transition = 'background-color 0.3s, color 0.3s';
+
+    // Hover effect for close button
+    closeModalButton.onmouseover = () => {
+      closeModalButton.style.backgroundColor = 'orangered';
+      closeModalButton.style.color = 'white';
+    };
+
+    closeModalButton.onmouseout = () => {
+      closeModalButton.style.backgroundColor = 'white';
+      closeModalButton.style.color = 'orangered';
+    };
+
     closeModalButton.onclick = () => {
       modalContainer.style.display = 'none';
     };
