7bd92da4182296f26301598cab53af5c88ae8598
Michael Oswald <145994175+OswaldMichael2008@users.noreply.github.com>
2025-01-20 21:39:23 +0100

    style weils ja sonst keiner macht @mörtn

--- Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
+++ Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d246c2471e425a27d6bd1f7b52ae49bf8a1dd88d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5120046f16cdd2c47ce84e0f35d4441e901b78aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
