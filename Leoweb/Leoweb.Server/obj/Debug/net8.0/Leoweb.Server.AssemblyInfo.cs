c8027a0ea95840762c8536b06459ea6e2ad29628
LangederDavid <d.langeder@students.htl-leonding.ac.at>
2025-01-21 16:08:31 +0100

    added minimal size for file display

--- Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
+++ Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a85d8bfdd8ad901aa82782f8176b4c5c79e51351")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+224259b3bd5a821a750c746791a6f7ac73f6b3ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
