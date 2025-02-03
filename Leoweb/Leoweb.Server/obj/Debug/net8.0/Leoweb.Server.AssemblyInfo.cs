11ccea77935fd658bf26d48aaae25ee3281a21ab
Manuel Groß <m.gross@htblaleonding.onmicrosoft.com>
2025-02-03 21:05:46 +0100

    dockerized frontend and backend

--- Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
+++ Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6cb921d1e01824660f2318049870628b94f5a3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+965deef8f92db34e35eaa901cbf1928e2eb59f81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
