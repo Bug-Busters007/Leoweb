4fd72a84de8b414120cb4909feb6367d536d4852
GrossManuelHTL <m.gross@htblaleonding.onmicrosoft.com>
2025-02-03 14:22:49 +0100

    added dockerfiles and nginx

--- Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
+++ Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4b4c88691ec13c718292ac0eb88d62d9bda4c42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08880163c7e4b6c13c17380969e7f6e33e296a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
