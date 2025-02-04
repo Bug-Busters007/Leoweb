edf5d7cd389d66e9395414304b6c9447ff68a761
GrossManuelHTL <m.gross@htblaleonding.onmicrosoft.com>
2025-02-04 08:56:27 +0100

    angular material

--- Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
+++ Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f640cc35c7261179bcc1b6860cf05df7dcc7148")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f97731537dc7340a9742bf9b8f39c69f360bf59e")]
->>>>>>> 9855fd3a230b206dfbfde9cf1d74eefab45268f9
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec99d8747362835cc365243a486328ba2ae93c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
