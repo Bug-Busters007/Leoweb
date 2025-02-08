46b8bced136b53e121d7124df556a6230b513f03
MoertendorferLukas <l.moertendorfer@students.htl-leonding.ac.at>
2025-02-08 01:43:37 +0100

    Started LeoPoll frontend

--- Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
+++ Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c344029d947280eb9ccbe08efb8d6d5f943fb55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d75808c5ee53a8a8d20647f301f33863c7aa99ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Leoweb/leoweb.client/src/app/leo-poll/leo-poll.component.ts
+++ Leoweb/leoweb.client/src/app/leo-poll/leo-poll.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environments";
+import {AuthService} from "../../services/auth.service";
+import {ApiService} from "../../services/api.service";
+import {Spinner} from "../components/spinner/spinner";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-leo-poll',
@@ -7,4 +14,28 @@ import { Component } from '@angular/core';
 })
 export class LeoPollComponent {
 
+  constructor(private http: HttpClient, private apiService: ApiService) {}
+
+  async ngOnInit() {
+    await this.getAllPolls();
+  }
+
+  async getAllPolls(): Promise<void>{
+    const pdiv = document.getElementById('pollsDiv');
+    const spinner = new Spinner(pdiv);
+    spinner.showSpinner();
+    const url = this.apiService.getApiUrl('Poll/all');
+    try {
+      const response = await this.http
+        .get(url)
+        .toPromise();
+      if (response) {
+        spinner.removeSpinner();
+        pdiv!.innerText = JSON.stringify(response);
+      }
+    } catch (error) {
+      console.error('Error getting all polls', error);
+      throw new Error('Failed to get all polls');
+    }
+  }
 }
