60de40cf9683accd4b18f039e26f1ccc9b43b2dc
LangederDavid <d.langeder@htblaleonding.onmicrosoft.com>
2025-02-13 16:54:01 +0100

    😤 SCHEISS STYLE FINALIZED! Nie wieder!!! 🎉 Nach gefühlt 100 Jahren Feintuning ist dieser verdammte Komponent endlich fertig. Kein Pixel mehr, kein Padding mehr, kein Bugfix mehr – ich bin RAUS! Wer hier noch was ändern will, macht’s allein. 😡🔥

--- Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
+++ Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b563de1efe0fb686c9573efc9d0b31be5cd95efa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1675a4dd496e9eb5d5156a3b9139b3f07f6137d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
