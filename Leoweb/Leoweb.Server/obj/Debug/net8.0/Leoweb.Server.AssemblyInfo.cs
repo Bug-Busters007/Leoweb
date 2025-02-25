38849f7c24446fa5309f3b907bc431594866f6c0
LangederDavid <d.langeder@htblaleonding.onmicrosoft.com>
2025-02-25 08:39:54 +0100

    changed style of filesearch for files to be correctly centered

--- Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
+++ Leoweb/Leoweb.Server/obj/Debug/net8.0/Leoweb.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f33c00a905e26ba134727f0b57bb66186925a547")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b9719db82857c6d59a8f0a9b37de86e2c6cf0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leoweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
