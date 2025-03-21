34cee8370acc54b8eecc9dc80e1ad69d46777a44
SchwendtbauerJulian <j.schwendtbauer@students.htl-leonding.ac.at>
2025-03-21 12:57:23 +0100

    begin style login´

--- Leoweb/leoweb.client/src/app/auth/login/login.component.ts
+++ Leoweb/leoweb.client/src/app/auth/login/login.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import {Router, RouterLink} from "@angular/router";
 import {AuthService} from "../../../services/auth.service";
 import {FormsModule} from "@angular/forms";
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrl: './login.component.css',
     imports: [
-        FormsModule,
-        RouterLink,
+      FormsModule,
+      RouterLink,
+      MatButtonModule,
+      MatFormField,
+      MatInputModule,
     ]
 })
 export class LoginComponent {
